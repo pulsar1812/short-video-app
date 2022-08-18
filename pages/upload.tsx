5595a5f4fd1d143a28eaa9d01e6114f851e1578d
pulsar1812 <cluster000@gmail.com>
2022-08-18 23:12:56 +0800

    Add POST action for video upload

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,6 +8,7 @@ import { SanityAssetDocument } from '@sanity/client'
 import useAuthStore from '../store/authStore'
 import { client } from '../utils/client'
 import { topics } from '../utils/constants'
+import { BASE_URL } from '../utils'
 
 export default function Upload() {
   const [isLoading, setIsLoading] = useState(false)
@@ -15,6 +16,12 @@ export default function Upload() {
     SanityAssetDocument | undefined
   >()
   const [wrongFileType, setWrongFileType] = useState(false)
+  const [caption, setCaption] = useState('')
+  const [topic, setTopic] = useState(topics[0].name)
+  const [savingPost, setSavingPost] = useState(false)
+
+  const { userProfile }: { userProfile: any } = useAuthStore()
+  const router = useRouter()
 
   const uploadVideo = async (e: any) => {
     const seletedFile = e.target.files[0]
@@ -36,9 +43,44 @@ export default function Upload() {
     }
   }
 
+  const handlePost = async () => {
+    if (caption && videoAsset?._id && topic) {
+      setSavingPost(true)
+
+      const document = {
+        _type: 'post',
+        caption,
+        video: {
+          _type: 'file',
+          asset: {
+            _type: 'reference',
+            _ref: videoAsset?._id,
+          },
+        },
+        userId: userProfile?._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: userProfile?._id,
+        },
+        topic,
+      }
+
+      await axios.post(`${BASE_URL}/api/post`, document)
+      router.push('/')
+    }
+  }
+
+  const handleDiscard = () => {}
+
   return (
-    <div className='flex justify-center w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8]'>
-      <div className='flex gap-6 flex-wrap justify-between items-center w-[60%] bg-white rounded-lg xl:h-[80vh] p-14 pt-6'>
+    <div
+      className='flex justify-center w-full h-full absolute left-0 
+    top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8]'
+    >
+      <div
+        className='flex gap-6 flex-wrap justify-center items-center 
+       bg-white rounded-lg xl:h-[80vh] p-14 pt-6'
+      >
         <div>
           <div>
             <p className='text-2xl font-bold'>Upload Video</p>
@@ -46,7 +88,12 @@ export default function Upload() {
               Post a video to your account
             </p>
           </div>
-          <div className='flex flex-col justify-center items-center border-dashed rounded-xl border-4 border-gray-200 outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100'>
+          <div
+            className='flex flex-col justify-center items-center 
+          border-dashed rounded-xl border-4 border-gray-200 outline-none 
+          mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 
+          hover:bg-gray-100'
+          >
             {isLoading ? (
               <p>Uploading...</p>
             ) : (
@@ -75,7 +122,10 @@ export default function Upload() {
                         Up to 10 minutes <br />
                         Less than 2GB
                       </p>
-                      <p className='bg-[#F51997] text-center mt-10 rounded text-white text-md font-medium p-2 w-52 outline-none'>
+                      <p
+                        className='bg-[#F51997] text-center mt-10 rounded text-white 
+                      text-md font-medium p-2 w-52 outline-none'
+                      >
                         Select File
                       </p>
                     </div>
@@ -101,23 +151,25 @@ export default function Upload() {
           <label className='text-md font-medium'>Caption</label>
           <input
             type='text'
-            value=''
-            onChange={() => {}}
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
             className='rounded outline-none text-md border-2 border-gray-200 p-2'
           />
 
-          <label className='text-md font-medium'>Choose a category</label>
+          <label className='text-md font-medium'>Choose a topic</label>
           <select
-            onChange={() => {}}
-            className='outline-none border-2 border-gray-200 text-md capitalize p-2 lg:p-4 rounded cursor-pointer'
+            onChange={(e) => setTopic(e.target.value)}
+            className='outline-none border-2 border-gray-200 text-md capitalize p-2 
+            lg:p-4 rounded cursor-pointer'
           >
-            {topics.map((topic) => (
+            {topics.map((item) => (
               <option
-                key={topic.name}
-                value={topic.name}
-                className='outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300'
+                key={item.name}
+                value={item.name}
+                className='outline-none capitalize bg-white text-gray-700 text-md p-2 
+                hover:bg-slate-300'
               >
-                {topic.name}
+                {item.name}
               </option>
             ))}
           </select>
@@ -125,15 +177,17 @@ export default function Upload() {
           <div className='flex gap-6 mt-10'>
             <button
               type='button'
-              onClick={() => {}}
-              className='w-28 lg:w-44 outline-none border-2 border-gray-300 text-md font-medium p-2 rounded'
+              onClick={handleDiscard}
+              className='w-28 lg:w-44 outline-none border-2 border-gray-300 text-md 
+              font-medium p-2 rounded'
             >
               Discard
             </button>
             <button
               type='button'
-              onClick={() => {}}
-              className='w-28 lg:w-44 outline-none bg-[#F51997] text-white text-md font-medium p-2 rounded'
+              onClick={handlePost}
+              className='w-28 lg:w-44 outline-none bg-[#F51997] text-white 
+              text-md font-medium p-2 rounded'
             >
               Post
             </button>
